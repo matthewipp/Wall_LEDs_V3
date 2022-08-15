28182628e7dbf319bfab7d121bcf5a878f7a6069
matthewipp <mattrippolito@gmail.com>
2022-08-14 22:18:48 -0400

    Added GPIO code

--- SPIManager.py
+++ SPIManager.py
@@ -8,15 +8,47 @@ This file contains functions that interact with the GPIO of the
 raspberry pi to implement SPI.
 """
 
-# import wiringpi
+import RPi.GPIO as GPIO
+import time
 
-SPI_COPI = 0
-SPI_CIPO = 0
-SPI_CS = 0
-SPI_SCK = 0
+COPI = 4
+CIPO = 27
+SCK = 21
+CS = 13
+SPI_SPEED = 500_000
 
+spi = spidev.SpiDev()
+
+# Init pins and set to starting value
 def init_SPI_pins():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(COPI, GPIO.OUT)
+    GPIO.setup(CIPO, GPIO.IN)
+    GPIO.setup(SCK, GPIO.OUT)
+    GPIO.setup(CS, GPIO.OUT)
+    GPIO.output(COPI, 0)
+    GPIO.output(SCK, 0)
+    GPIO.output(CS, 1)
+    GPIO.output(CS, 1)
     return
 
+# send data to raspberry pi
 def send_data(data):
+    # Enable sending data
+    GPIO.output(CS, 0)
+    time.sleep(25)
+    # Send all data
+    for word in data:
+        bit_selector = 0b1000_0000
+        for i in range(8):
+            if word & bit_selector:
+                GPIO.output(COPI, 1)
+            else:
+                GPIO.output(COPI, 0)
+            # python too slow for sleep?
+            # time.sleep(0.001)
+            bit_selector = bit_selector >> 1
+            GPIO.output(SCK, 1)
+            GPIO.output(SCK, 0)
+    GPIO.output(CS, 1)        
     return
